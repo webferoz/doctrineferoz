a6066becea48ab1546a0c3a9ea87bf2dc2c6c7df
OpenShift guest <pedro@webferoz.com.br>
2013-10-03 23:44:54 -0400

    add views and edit controller album for crud using doctrine entitys

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -1,24 +1,124 @@
 <?php
+ 
 namespace Album\Controller;
-
-use Zend\Mvc\Controller\AbstractActionController;
+ 
+use Zend\Mvc\Controller\ActionController;
 use Zend\View\Model\ViewModel;
-
-class AlbumController extends AbstractActionController
+use Album\Form\AlbumForm;
+use Doctrine\ORM\EntityManager;
+use Album\Entity\Album;
+ 
+class AlbumController extends ActionController
 {
+    /**
+     * @var Doctrine\ORM\EntityManager
+     */
+    protected $em;
+ 
+    public function setEntityManager(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+ 
+    public function getEntityManager()
+    {
+        if (null === $this->em) {
+            $this->em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+        }
+        return $this->em;
+    } 
+ 
     public function indexAction()
     {
+        return new ViewModel(array(
+            'albums' => $this->getEntityManager()->getRepository('Album\Entity\Album')->findAll() 
+        ));
     }
-
+ 
     public function addAction()
     {
+        $form = new AlbumForm();
+        $form->get('submit')->setAttribute('label', 'Add');
+ 
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $album = new Album();
+            
+            $form->setInputFilter($album->getInputFilter());
+            $form->setData($request->post());
+            if ($form->isValid()) { 
+                $album->populate($form->getData()); 
+                $this->getEntityManager()->persist($album);
+                $this->getEntityManager()->flush();
+ 
+                // Redirect to list of albums
+                return $this->redirect()->toRoute('album'); 
+            }
+        }
+ 
+        return array('form' => $form);
     }
-
+ 
     public function editAction()
     {
+        $id = (int)$this->getEvent()->getRouteMatch()->getParam('id');
+        if (!$id) {
+            return $this->redirect()->toRoute('album', array('action'=>'add'));
+        } 
+        $album = $this->getEntityManager()->find('Album\Entity\Album', $id);
+ 
+        $form = new AlbumForm();
+        $form->setBindOnValidate(false);
+        $form->bind($album);
+        $form->get('submit')->setAttribute('label', 'Edit');
+        
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $form->setData($request->post());
+            if ($form->isValid()) {
+                $form->bindValues();
+                $this->getEntityManager()->flush();
+ 
+                // Redirect to list of albums
+                return $this->redirect()->toRoute('album');
+            }
+        }
+ 
+        return array(
+            'id' => $id,
+            'form' => $form,
+        );
     }
-
+ 
     public function deleteAction()
     {
+        $id = (int)$this->getEvent()->getRouteMatch()->getParam('id');
+        if (!$id) {
+            return $this->redirect()->toRoute('album');
+        }
+ 
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $del = $request->post()->get('del', 'No');
+            if ($del == 'Yes') {
+                $id = (int)$request->post()->get('id');
+                $album = $this->getEntityManager()->find('Album\Entity\Album', $id);
+                if ($album) {
+                    $this->getEntityManager()->remove($album);
+                    $this->getEntityManager()->flush();
+                }
+            }
+ 
+            // Redirect to list of albums
+            return $this->redirect()->toRoute('default', array(
+                'controller' => 'album',
+                'action'     => 'index',
+            ));
+        }
+ 
+        return array(
+            'id' => $id,
+            'album' => $this->getEntityManager()->find('Album\Entity\Album', $id)->getArrayCopy()
+        );
     }
 }
