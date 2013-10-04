343482cfb5105445a1a2dc2bed417bdd8e01f598
OpenShift guest <pedro@webferoz.com.br>
2013-10-03 23:56:44 -0400

    edit getEntity

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -18,7 +18,7 @@ class AlbumController extends AbstractActionController
     public function getEntityManager()
     {
         if (null === $this->em) {
-            $$this->em = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+            $this->em = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
         }
         return $this->em;
     } 
